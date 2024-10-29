2eb9f9a7e2a07fb563409a983cac547c26e49517
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-10-29 13:36:16 +0000

    Import everything needed in views.py

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.template.response import TemplateResponse
+from django.views.decorators.http import require_POST
+from .forms import CommentForm
+from .models import Blog, Comment, Like
 
 # Create your views here.

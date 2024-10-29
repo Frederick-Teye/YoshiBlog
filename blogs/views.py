8a551962647bd37ccd6a656e3737aa07add4bb46
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-10-29 13:49:00 +0000

    Define blog_list_view

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import JsonResponse
 from django.template.response import TemplateResponse
 from django.views.decorators.http import require_POST
@@ -5,3 +6,12 @@ from .forms import CommentForm
 from .models import Blog, Comment, Like
 
 # Create your views here.
+
+
+def blog_list_view(request):
+    blogs = Blog.objects.all()
+    paginator = Paginator(blogs, 5)  # Show 5 blogs per page.
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    template = "blog_list.html"
+    TemplateResponse(request, template, {"page_obj": page_obj})

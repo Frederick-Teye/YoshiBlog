c116355d0175d16d5ceee9d2b0dd47046c23d585
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-10-30 09:14:08 +0000

    Set urlpatterns for the created blog views.

---
+++ blogs/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import (
+    blog_create_view,
+    blog_detail_view,
+    blog_list_view,
+)
+
+urlpatterns = [
+    path("<int:pk>", blog_detail_view, name="blog_detail"),
+    path("new/", blog_create_view, name="blog_new"),
+    path("", blog_list_view, name="blog_list"),
+]

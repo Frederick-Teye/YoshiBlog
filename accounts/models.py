a2d81b28c70e9464a3dab4dbc38c270252336e3c
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-10-31 13:18:56 +0000

    Modify CustomUser model class.

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class CustomUser(AbstractUser):
-    pass
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_short_name(self):
+        return self.first_name
 
     def __str__(self):
         return self.email

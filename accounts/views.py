d370400d21df8c25f7fe0973446c73062259c7a4
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-10-31 13:41:35 +0000

    Add profile update view logic.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileUpdateForm
 
 # Create your views here.
+
+
+@login_required
+def profile_update(request):
+    if request.method == "POST":
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            # change redirect to profile page in the future.
+            return redirect("home")
+    else:
+        form = ProfileUpdateForm(instance=request.user)
+
+    return render(request, "accounts/profile_update.html", {"form": form})

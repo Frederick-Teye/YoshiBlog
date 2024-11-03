20737de33c8589777e369cb611ab04fd71320adf
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-11-03 19:39:23 +0000

    Use js to manipulate blog list link

---
+++ static/js/base.js
@@ -0,0 +1,5 @@
+const addBlogContainer = document.getElementById("text-field-container");
+
+addBlogContainer.addEventListener('click', function() {
+    document.getElementById('add-blog-link').click();
+});

f5aa6273603bfe4327a864e879b86044e5343756
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-11-03 21:08:54 +0000

    Add blog_update_view to urlspattern

--- blogs/urls.py
+++ blogs/urls.py
@@ -4,10 +4,12 @@ from .views import (
     blog_create_view,
     blog_detail_view,
     blog_list_view,
+    blog_update_view,
 )
 
 urlpatterns = [
     path("<int:pk>/", blog_detail_view, name="blog_detail"),
+    path("<int:pk>/edit/", blog_update_view, name='blog_edit'),
     path("new/", blog_create_view, name="blog_new"),
     path("", blog_list_view, name="blog_list"),
 ]

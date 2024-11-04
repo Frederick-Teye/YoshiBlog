94a48d9f75c6db377cf7e12eb2471547b16f3399
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-11-04 21:24:33 +0000

    Import blog_delete_view

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import (
     blog_create_view,
+    blog_delete_view,
     blog_detail_view,
     blog_list_view,
     blog_update_view,
@@ -9,7 +10,7 @@ from .views import (
 
 urlpatterns = [
     path("<int:pk>/", blog_detail_view, name="blog_detail"),
-    path("<int:pk>/edit/", blog_update_view, name='blog_edit'),
+    path("<int:pk>/edit/", blog_update_view, name="blog_edit"),
     path("new/", blog_create_view, name="blog_new"),
     path("", blog_list_view, name="blog_list"),
 ]

75702d45014ea1151ac8f4b9f1f43ec058c88897
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-11-04 22:38:50 +0000

    Add js code to make blog delete button function.

--- static/js/base.js
+++ static/js/base.js
@@ -3,3 +3,9 @@ const addBlogContainer = document.getElementById("text-field-container");
 addBlogContainer.addEventListener('click', function() {
     document.getElementById('add-blog-link').click();
 });
+
+
+// Delete blog in blog list view
+function deleteBlog(id){
+     document.getElementById('delete-blog-link-'+id).click();
+}

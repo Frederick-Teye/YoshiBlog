95586ac1d02b84a7a98a922a3d035e66b0e75082
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-11-15 04:38:49 +0000

    Create total_blog_likes total_comment_likes & total_comments templatetags

--- blogs/templatetags/custom_tags.py
+++ blogs/templatetags/custom_tags.py
@@ -16,3 +16,21 @@ def user_likes_comment(context, pk):
     comment = get_object_or_404(Comment.objects.all(), pk=pk)
     user = context["request"].user
     return comment.comment_likes.filter(id=user.id).exists()
+
+
+@register.simple_tag()
+def total_blog_likes(pk):
+    blog = get_object_or_404(Blog.objects.all(), pk=pk)
+    return blog.blog_likes.count()
+
+
+@register.simple_tag()
+def total_comment_likes(pk):
+    comment = get_object_or_404(Comment.objects.all(), pk=pk)
+    return comment.comment_likes.count()
+
+
+@register.simple_tag()
+def total_comments(pk):
+    blog_instance = Blog.objects.get(pk=pk)
+    return blog_instance.comments.count()

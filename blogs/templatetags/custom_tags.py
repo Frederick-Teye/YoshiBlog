7a768722769159cf08c1e00659b4ac870f27bda7
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-11-15 05:25:43 +0000

    Refactor code.

--- blogs/templatetags/custom_tags.py
+++ blogs/templatetags/custom_tags.py
@@ -4,30 +4,31 @@ from blogs.models import Blog, Comment
 
 register = template.Library()
 
+
 @register.simple_tag(takes_context=True)
 def user_likes_blog(context, pk):
     blog = get_object_or_404(Blog.objects.all(), pk=pk)
     user = context["request"].user
-    return blog.blog_likes.filter(id=user.id).exists()
+    return blog.likes.filter(id=user.id).exists()
 
 
 @register.simple_tag(takes_context=True)
 def user_likes_comment(context, pk):
     comment = get_object_or_404(Comment.objects.all(), pk=pk)
     user = context["request"].user
-    return comment.comment_likes.filter(id=user.id).exists()
+    return comment.likes.filter(id=user.id).exists()
 
 
 @register.simple_tag()
 def total_blog_likes(pk):
     blog = get_object_or_404(Blog.objects.all(), pk=pk)
-    return blog.blog_likes.count()
+    return blog.likes.count()
 
 
 @register.simple_tag()
 def total_comment_likes(pk):
     comment = get_object_or_404(Comment.objects.all(), pk=pk)
-    return comment.comment_likes.count()
+    return comment.likes.count()
 
 
 @register.simple_tag()

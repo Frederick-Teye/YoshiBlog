32cf64b81afbb4808347e150e93abe150ef724b2
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-11-22 23:56:51 +0000

    Add total_likes property to Comment model

--- blogs/models.py
+++ blogs/models.py
@@ -36,5 +36,9 @@ class Comment(models.Model):
         related_name="comment_likes",
     )
 
+    @property
+    def total_likes(self):
+        return self.likes.count()
+
     def __str__(self):
         return self.comment

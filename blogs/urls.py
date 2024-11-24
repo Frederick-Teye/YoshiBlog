0070a3a683e4ac8cd4e9fbc9beb599b388604cbb
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-11-24 15:57:42 +0000

    Add blog_like_view to urlpatterns

--- blogs/urls.py
+++ blogs/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path("<int:pk>/", blog_detail_view, name="blog_detail"),
     path("<int:pk>/delete/", blog_delete_view, name="blog_delete"),
     path("<int:pk>/edit/", blog_update_view, name="blog_edit"),
+    path("<int:ip>/like/", blog_like_view, name="blog_like"),
     path("new/", blog_create_view, name="blog_new"),
     path("", blog_list_view, name="blog_list"),
 ]

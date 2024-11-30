6fdb1e6150ce1123c10568e0e1e7f7acb9af0427
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-11-30 22:08:42 +0000

    Add comment_delete & comment urls to urlpatterns

--- blogs/urls.py
+++ blogs/urls.py
@@ -7,6 +7,8 @@ from .views import (
     blog_list_view,
     blog_update_view,
     blog_like_view,
+    comment_delete,
+    comment_edit,
 )
 
 urlpatterns = [
@@ -14,7 +16,9 @@ urlpatterns = [
     path("<int:pk>/delete/", blog_delete_view, name="blog_delete"),
     path("<int:pk>/edit/", blog_update_view, name="blog_edit"),
     path("<int:pk>/like/", blog_like_view, name="blog_like"),
-    path('<int:pk>/list_like/', blog_like_view, name="list_blog_like"),
+    path("<int:pk>/list_like/", blog_like_view, name="list_blog_like"),
+    path("<int:pk>/<int:comment_pk>/delete", comment_delete, name="comment_delete"),
+    path("<int:pk>/<int:comment_pk>/edit", comment_edit, name="comment_edit"),
     path("new/", blog_create_view, name="blog_new"),
     path("", blog_list_view, name="blog_list"),
 ]

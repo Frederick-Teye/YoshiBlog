34dbf1b5456981f4db25b066dfdf920db48d7047
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-21 19:24:06 +0000

    Change CommentForm to inherit from ModelForm directly

--- blogs/forms.py
+++ blogs/forms.py
@@ -3,7 +3,7 @@ from django.forms import ModelForm, Textarea
 from .models import Comment, Blog
 
 
-class CommentForm(forms.ModelForm):
+class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = ("comment",)

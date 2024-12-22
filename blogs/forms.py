64cbcd53d6d9d5ac9f771e92aa6548992ea68c5f
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-12-22 11:27:51 +0000

    Modify widget of commentForm()

--- blogs/forms.py
+++ blogs/forms.py
@@ -7,6 +7,15 @@ class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = ("comment",)
+        widgets = {
+            "comment": Textarea(
+                attrs={
+                    "cols": 80,
+                    "rows": 1,
+                    "class": "no-scrollbars",
+                }
+            ),
+        }
 
 
 class BlogForm(ModelForm):

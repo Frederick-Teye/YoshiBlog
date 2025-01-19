dd5909374aa42a743e18c37f20eccbf7297f6e3e
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2025-01-19 23:59:47 +0000

    Create BlogAdmin class

--- blogs/admin.py
+++ blogs/admin.py
@@ -4,4 +4,16 @@ from .models import Blog, Comment
 # Register your models here.
 
 admin.site.register(Blog)
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ["title", "get_tags"]
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related("tags")
+
+    def get_tags(self, obj):
+        return ", ".join(o for o in obj.tags.names())
+
+
 admin.site.register(Comment)

836a8b1ed9d7204380edf78f0f64e7a069056fad
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2025-01-24 00:05:49 +0000

    Remove unused imports

--- blogs/views.py
+++ blogs/views.py
@@ -1,12 +1,9 @@
 import logging
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.template.response import TemplateResponse
 from django.utils.text import slugify
-from django.views.decorators.http import require_POST
-from django.views.decorators.csrf import ensure_csrf_cookie
 from .forms import CommentForm, BlogForm
 from .models import Blog, Comment
 

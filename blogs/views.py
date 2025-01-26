0e90e846716ef8583090a0a98c0a190f1e81d017
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2025-01-26 15:10:37 +0000

    Import deepcopy from copy

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,6 @@
 import logging
 import nh3
+from copy import deepcopy
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render

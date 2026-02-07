2d0fa0daceb7c8e0ee0153cae455ab31fc1e9d38
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2026-02-07 20:12:03 +0000

    Guard debug toolbar urls

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+import importlib.util
 from django.contrib import admin
 from django.urls import path, include
 from accounts.views import profile_update
@@ -11,9 +12,7 @@ urlpatterns = [
     path("", include("pages.urls")),
 ]
 
-if settings.DEBUG:
+if settings.DEBUG and importlib.util.find_spec("debug_toolbar") is not None:
     import debug_toolbar
 
-    urlpatterns = [
-        path("__debug__/", include(debug_toolbar.urls)),
-    ] + urlpatterns
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns

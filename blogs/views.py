67b65fa87bbaf50be02ee73164edf8122bacf0bb
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2026-02-08 22:05:52 +0000

    Remove dsql retry wrapper utility code

--- blogs/views.py
+++ blogs/views.py
@@ -1,21 +1,19 @@
 import logging
 import nh3
-from copy import deepcopy
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, redirect
 from django.template.response import TemplateResponse
 from django.utils.text import slugify
 from .forms import CommentForm, BlogForm
 from .models import Blog, Comment
-from .dsql_utils import dsql_retry
 
 logger = logging.getLogger(__name__)
 
 
 def blog_list_view(request):
     blogs = Blog.objects.all().order_by("-date")
-    paginator = Paginator(blogs, 3)  # Show 3 blogs per page.
+    paginator = Paginator(blogs, 3)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     tag_names = list(Blog.objects.values_list("tags__name", flat=True).distinct())
@@ -52,12 +50,10 @@ def blog_detail_view(request, pk, blog_slug):
         "did_user_comment": did_user_comment,
         "is_ordered_by_likes": is_ordered_by_likes,
     }
-    template = "blog_detail.html"
-    return TemplateResponse(request, template, context)
+    return TemplateResponse(request, "blog_detail.html", context)
 
 
 @login_required
-@dsql_retry()
 def blog_create_view(request):
     if request.method == "POST":
         form = BlogForm(request.POST)
@@ -73,7 +69,7 @@ def blog_create_view(request):
                 counter += 1
                 if counter == 1:
                     original_slug = original_slug + "-" + str(counter)
-                elif 1 < counter and counter <= 10:
+                elif 1 < counter <= 10:
                     original_slug = original_slug[:-1] + str(counter)
                 elif counter > 10:
                     original_slug = original_slug[:-2] + str(counter)
@@ -87,12 +83,10 @@ def blog_create_view(request):
             )
     else:
         form = BlogForm()
-
     return TemplateResponse(request, "blog_new.html", {"form": form})
 
 
 @login_required
-@dsql_retry()
 def blog_update_view(request, pk):
     blog = get_object_or_404(Blog.objects.all(), pk=pk)
     if request.method == "POST":
@@ -112,12 +106,10 @@ def blog_update_view(request, pk):
             )
     else:
         form = BlogForm(instance=blog)
-
     return TemplateResponse(request, "blog_edit.html", {"form": form})
 
 
 @login_required
-@dsql_retry()
 def blog_delete_view(request, pk):
     blog = get_object_or_404(Blog.objects.all(), pk=pk)
     blog.delete()
@@ -125,56 +117,39 @@ def blog_delete_view(request, pk):
 
 
 @login_required
-@dsql_retry()
 def blog_like_view(request, pk):
+    blog = get_object_or_404(Blog.objects.all(), id=request.POST.get("blog_id"))
+    is_liked = False
+    if blog.likes.filter(id=request.user.id).exists():
+        blog.likes.remove(request.user)
+    else:
+        blog.likes.add(request.user)
+        is_liked = True
+
+    context = {
+        "blog": blog,
+        "is_liked": is_liked,
+        "total_likes": blog.likes.count(),
+    }
+
     if request.path_info == "/blogs/" + str(pk) + "/like/":
-        blog = get_object_or_404(Blog.objects.all(), id=request.POST.get("blog_id"))
-        is_liked = False
-        if blog.likes.filter(id=request.user.id).exists():
-            blog.likes.remove(request.user)
-            is_liked = False
-        else:
-            blog.likes.add(request.user)
-            is_liked = True
-        context = {
-            "blog": blog,
-            "is_liked": is_liked,
-            "total_likes": blog.likes.count(),
-        }
         template = "blog_detail_components/reaction_section.html"
     else:
-        blog = get_object_or_404(Blog.objects.all(), id=request.POST.get("blog_id"))
-        is_liked = False
-        if blog.likes.filter(id=request.user.id).exists():
-            blog.likes.remove(request.user)
-            is_liked = False
-        else:
-            blog.likes.add(request.user)
-            is_liked = True
-        context = {
-            "blog": blog,
-            "is_liked": is_liked,
-            "total_likes": blog.likes.count(),
-        }
         template = "blog_list_components/reaction_section.html"
     return TemplateResponse(request, template, context)
 
 
 @login_required
-@dsql_retry()
 def comment_delete_view(request, pk, blog_slug, comment_pk):
     blog = get_object_or_404(Blog.objects.all(), pk=pk)
-    check_and_log_wrong_slug(request, blog, blog_slug)
     comment = get_object_or_404(Comment.objects.all(), blog=blog, pk=comment_pk)
     comment.delete()
     return redirect("blog_detail", pk=pk, blog_slug=blog_slug)
 
 
 @login_required
-@dsql_retry()
 def comment_update_view(request, pk, blog_slug, comment_pk):
     blog = get_object_or_404(Blog.objects.all(), pk=pk)
-    check_and_log_wrong_slug(request, blog, blog_slug)
     comment = get_object_or_404(Comment.objects.all(), blog=blog, pk=comment_pk)
     if request.method == "POST":
         form = CommentForm(request.POST, instance=comment)
@@ -186,19 +161,14 @@ def comment_update_view(request, pk, blog_slug, comment_pk):
             return redirect("blog_detail", pk=blog.pk, blog_slug=blog_slug)
     else:
         form = CommentForm(instance=comment)
-
     return TemplateResponse(
         request, "blog_detail_components/comment_edit_form.html", {"form": form}
     )
 
 
 @login_required
-@dsql_retry()
-def comment_create_view(
-    request, pk, blog_slug
-):  # pk is the pk of the blog which comment belong to
+def comment_create_view(request, pk, blog_slug):
     blog = get_object_or_404(Blog.objects.all(), pk=pk)
-    check_and_log_wrong_slug(request, blog, blog_slug)
     form = CommentForm(request.POST)
     if form.is_valid():
         sanitize_comment = sanitize_input(form.instance.comment)
@@ -214,14 +184,10 @@ def comment_create_view(
 
 
 @login_required
-@dsql_retry()
 def comment_like_view(request, pk, blog_slug, comment_pk):
     blog = get_object_or_404(Blog.objects.all(), pk=pk)
-    check_and_log_wrong_slug(request, blog, blog_slug)
     comment = get_object_or_404(Comment.objects.all(), blog=blog, pk=comment_pk)
-    if (
-        request.user in comment.likes.all()
-    ):  # new style I learnt to filter which easy to understand by just looking
+    if request.user in comment.likes.all():
         comment.likes.remove(request.user)
     else:
         comment.likes.add(request.user)
@@ -236,8 +202,7 @@ def check_and_log_wrong_slug(request, blog, blog_slug):
     if blog_slug != blog.slug:
         referrer = request.META.get("HTTP_REFERRER", "No referrer")
         logger.error(
-            "A bad slug was encountered: '%s'. The expected slug was '%s'. "
-            + "The referring location was %s.",
+            "A bad slug was encountered: '%s'. Expected: '%s'. Referrer: %s",
             blog_slug,
             blog.slug,
             referrer,
@@ -246,15 +211,13 @@ def check_and_log_wrong_slug(request, blog, blog_slug):
 
 @login_required
 def list_blog_tagged(request, tag_name):
-    # list view that list blogs with a particular tagname
     blogs = Blog.objects.filter(tags__name__in=[tag_name]).order_by("-date")
-    paginator = Paginator(blogs, 3)  # Show 3 blogs per page.
+    paginator = Paginator(blogs, 3)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     tag_names = list(Blog.objects.values_list("tags__name", flat=True).distinct())
-    template = "blog_list.html"
     return TemplateResponse(
-        request, template, {"page_obj": page_obj, "tags": tag_names}
+        request, "blog_list.html", {"page_obj": page_obj, "tags": tag_names}
     )
 
 

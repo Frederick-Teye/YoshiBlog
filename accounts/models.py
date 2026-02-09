33d7d6d9c7718f85b6c32b20f4ff576269425ca6
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2026-02-09 23:55:14 +0000

    Update get_full_name method of CustomUser class in accounts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,11 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     def get_full_name(self):
-        return f"{self.first_name} {self.last_name}"
+        if self.first_name and self.last_name:
+            return f"{self.first_name} {self.last_name}"
+        if self.first_name:
+            return self.first_name
+        return self.username or self.email.split('@')[0]
 
     def get_short_name(self):
         return self.first_name

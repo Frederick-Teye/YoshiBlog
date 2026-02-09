457f4f23c2ef12c9ecde5a35ce0ad75fb1b4d4d0
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2026-02-09 04:28:25 +0000

    Add shareBlogPost function to login_required.js and improve logging in base.js

--- static/js/base.js
+++ static/js/base.js
@@ -1,23 +1,23 @@
-$(document).ready(function() {
+$(document).ready(function () {
   function handleDetailViewLike() {
-    $(document).on('click', '#detail-view-like', function() {
-      var pk = $(this).data('id');
-      var csrfToken = $(this).data('csrf-token');
+    $(document).on("click", "#detail-view-like", function () {
+      var pk = $(this).data("id");
+      var csrfToken = $(this).data("csrf-token");
 
       $.ajax({
-        type: 'POST',
-        url: $(this).data('href'),
+        type: "POST",
+        url: $(this).data("href"),
         headers: {
-          'X-CSRFToken': csrfToken
+          "X-CSRFToken": csrfToken,
         },
         data: {
-          "blog_id": pk,
+          blog_id: pk,
         },
-        success: function(data) {
+        success: function (data) {
           var id = "#blog_" + pk + "_detail_reaction_section";
           $(id).html(data);
         },
-        error: function(rs, e) {
+        error: function (rs, e) {
           console.log(rs.responseText);
         },
       });
@@ -25,26 +25,26 @@ $(document).ready(function() {
   }
 
   function handleListViewLike() {
-    $(document).on('click', '.blog-like-section', function() {
+    $(document).on("click", ".blog-like-section", function () {
       var $this = $(this);
-      var pk = $this.data('id');
-      var csrfToken = $this.data('csrf-token');
-      let urlRoute = $this.data('href');
+      var pk = $this.data("id");
+      var csrfToken = $this.data("csrf-token");
+      let urlRoute = $this.data("href");
 
       $.ajax({
-        type: 'POST',
+        type: "POST",
         url: urlRoute,
         headers: {
-          'X-CSRFToken': csrfToken
+          "X-CSRFToken": csrfToken,
         },
         data: {
-          "blog_id": pk,
+          blog_id: pk,
         },
-        success: function(data) {
+        success: function (data) {
           var id = "#blog_" + pk + "_list_item_reaction_section";
-          $(id).html(data)
+          $(id).html(data);
         },
-        error: function(rs, e) {
+        error: function (rs, e) {
           console.log(rs.responseText);
         },
       });
@@ -52,84 +52,99 @@ $(document).ready(function() {
   }
 
   handleDetailViewLike();
-  handleListViewLike();  
+  handleListViewLike();
 });
-
 
 const addBlogContainer = document.getElementById("text-field-container");
 
 if (addBlogContainer) {
-  addBlogContainer.addEventListener('click', function() {
-    document.getElementById('add-blog-link').click();
+  addBlogContainer.addEventListener("click", function () {
+    document.getElementById("add-blog-link").click();
   });
 }
-
 
 // Delete blog in blog list view
 // function deleteBlog(id){
 //      document.getElementById('delete-blog-link-'+id).click();
 // }
 
-
 window.addEventListener("resize", changeCommentFieldText);
-const commentFieldText = document.getElementsByClassName('comment-text-field');
+const commentFieldText = document.getElementsByClassName("comment-text-field");
 function changeCommentFieldText() {
-    if (screen.width <= 390){
-        for (let i = 0; i < commentFieldText.length; i++){
-            commentFieldText[i].innerText = "Comment here...";
-        }
-    } else {
-        for (let i = 0; i < commentFieldText.length; i++){
-            commentFieldText[i].innerText = "Write your comment here ...";
-        }
+  if (screen.width <= 390) {
+    for (let i = 0; i < commentFieldText.length; i++) {
+      commentFieldText[i].innerText = "Comment here...";
     }
+  } else {
+    for (let i = 0; i < commentFieldText.length; i++) {
+      commentFieldText[i].innerText = "Write your comment here ...";
+    }
+  }
 }
 
-
-function submitCommentForm(formObject){
+function submitCommentForm(formObject) {
   const form = $(formObject);
   let urlRoute = $(formObject).data("href");
   let blog_pk = $(formObject).data("id");
 
   $.ajax({
-      type: 'POST',
-      url: urlRoute,
-      data: $(form).serialize(),
+    type: "POST",
+    url: urlRoute,
+    data: $(form).serialize(),
 
-      success: function(data) {
-        var commentSection = $('#comment-section-' + blog_pk);
-        // Get the value of the data-has-comments attribute
-        var hasComments = commentSection.data('has-comments');
-        // Check if there are comments
-        if (hasComments === true) {
-            commentSection.prepend(data);
-        } else {
-            hasComments = true;
-            commentSection.html(data);
-        }
-      },
-      error: function(rs, e) {
-        console.log(rs.responseText);
-      },
-    });
+    success: function (data) {
+      var commentSection = $("#comment-section-" + blog_pk);
+      // Get the value of the data-has-comments attribute
+      var hasComments = commentSection.data("has-comments");
+      // Check if there are comments
+      if (hasComments === true) {
+        commentSection.prepend(data);
+      } else {
+        hasComments = true;
+        commentSection.html(data);
+      }
+    },
+    error: function (rs, e) {
+      console.log(rs.responseText);
+    },
+  });
 }
 
 // Function to share the blog post
 function shareBlogPost(title, url) {
+  console.log("shareBlogPost called with title:", title, "url:", url);
   if (navigator.share) {
-    navigator.share({
-      title: title,
-      url: url
-    }).catch(console.error);
+    console.log("Using navigator.share");
+    navigator
+      .share({
+        title: title,
+        url: url,
+      })
+      .then(() => {
+        console.log("Share successful");
+      })
+      .catch((error) => {
+        console.error("Share failed:", error);
+      });
   } else {
+    console.log("Navigator.share not available, trying clipboard");
     // Fallback: copy to clipboard
-    navigator.clipboard.writeText(url).then(function() {
-      alert('Link copied to clipboard: ' + url);
-    }).catch(function(err) {
-      console.error('Failed to copy: ', err);
-      // Fallback: open a new window or something
-      window.open('mailto:?subject=' + encodeURIComponent(title) + '&body=' + encodeURIComponent(url));
-    });
+    navigator.clipboard
+      .writeText(url)
+      .then(function () {
+        console.log("Link copied to clipboard");
+        alert("Link copied to clipboard: " + url);
+      })
+      .catch(function (err) {
+        console.error("Failed to copy:", err);
+        // Fallback: open a new window or something
+        console.log("Opening mailto");
+        window.open(
+          "mailto:?subject=" +
+            encodeURIComponent(title) +
+            "&body=" +
+            encodeURIComponent(url),
+        );
+      });
   }
 }
-
--- static/js/login_required.js
+++ static/js/login_required.js
@@ -1,71 +1,106 @@
-$(document).ready(function() {
-    function handleDetailViewLike() {
-        $(document).on('click', '#detail-view-like', function() {
-            let nextHref = window.location.href;
-            window.location.href = "/accounts/login/?next=" + nextHref + "#detail-view-like";
-        });
-    }
+$(document).ready(function () {
+  function handleDetailViewLike() {
+    $(document).on("click", "#detail-view-like", function () {
+      let nextHref = window.location.href;
+      window.location.href =
+        "/accounts/login/?next=" + nextHref + "#detail-view-like";
+    });
+  }
 
-    function handleListViewLike() {
-        $('.blog-like-section').click(function() {
-            let pk = $(this).data('id');
-            let nextHref = window.location.href;    
-            let blogReactionId = "#blog_" + pk + "_list_item_reaction_section";
-            window.location.href = "/accounts/login/?next=" + nextHref + blogReactionId;
-        });
-    }
+  function handleListViewLike() {
+    $(".blog-like-section").click(function () {
+      let pk = $(this).data("id");
+      let nextHref = window.location.href;
+      let blogReactionId = "#blog_" + pk + "_list_item_reaction_section";
+      window.location.href =
+        "/accounts/login/?next=" + nextHref + blogReactionId;
+    });
+  }
 
-    function autoExpandTextArea() {
-        $('.no-scrollbars').on('keyup keypress', function() {
-            $(this).height(0);
-            $(this).height(this.scrollHeight);
-        });
-    }
+  function autoExpandTextArea() {
+    $(".no-scrollbars").on("keyup keypress", function () {
+      $(this).height(0);
+      $(this).height(this.scrollHeight);
+    });
+  }
 
-    handleDetailViewLike();
-    handleListViewLike();
-    autoExpandTextArea();
+  handleDetailViewLike();
+  handleListViewLike();
+  autoExpandTextArea();
 });
-
 
 const addBlogContainer = document.getElementById("text-field-container");
 
 if (addBlogContainer) {
-    addBlogContainer.addEventListener('click', function() {
-        let nextHref = window.location.href;
-        window.location.href = "/accounts/login/?next=" + nextHref;
-    });
+  addBlogContainer.addEventListener("click", function () {
+    let nextHref = window.location.href;
+    window.location.href = "/accounts/login/?next=" + nextHref;
+  });
 }
-
-
 
 window.addEventListener("resize", changeCommentFieldText);
-const commentFieldText = document.getElementsByClassName('comment-text-field');
+const commentFieldText = document.getElementsByClassName("comment-text-field");
 function changeCommentFieldText() {
-    if (screen.width <= 390){
-        for (let i = 0; i < commentFieldText.length; i++){
-            commentFieldText[i].innerText = "Comment here...";
-        }
-    } else {
-        for (let i = 0; i < commentFieldText.length; i++){
-            commentFieldText[i].innerText = "Write your comment here ...";
-        }
+  if (screen.width <= 390) {
+    for (let i = 0; i < commentFieldText.length; i++) {
+      commentFieldText[i].innerText = "Comment here...";
     }
+  } else {
+    for (let i = 0; i < commentFieldText.length; i++) {
+      commentFieldText[i].innerText = "Write your comment here ...";
+    }
+  }
 }
 
-
-function submitCommentForm(formObject){
-    let nextHref = window.location.href;
-    let blog_pk = formObject.getAttribute("data-id");
-    let formId = "#comment-input-box-" + blog_pk;
-    window.location.href = "/accounts/login/?next=" + nextHref + formId;
+function submitCommentForm(formObject) {
+  let nextHref = window.location.href;
+  let blog_pk = formObject.getAttribute("data-id");
+  let formId = "#comment-input-box-" + blog_pk;
+  window.location.href = "/accounts/login/?next=" + nextHref + formId;
 }
-
 
 function submitLike(commentLikeButtonObject) {
-    let nextHref = window.location.href;
-    let commentIdInDatabase = commentLikeButtonObject.getAttribute("data-id");
-    let commentIdInHtml = "#comment-reaction-" + commentIdInDatabase;
-    window.location.href = "/accounts/login/?next=" + nextHref + commentIdInHtml;
+  let nextHref = window.location.href;
+  let commentIdInDatabase = commentLikeButtonObject.getAttribute("data-id");
+  let commentIdInHtml = "#comment-reaction-" + commentIdInDatabase;
+  window.location.href = "/accounts/login/?next=" + nextHref + commentIdInHtml;
 }
 
+// Function to share the blog post
+function shareBlogPost(title, url) {
+  console.log("shareBlogPost called with title:", title, "url:", url);
+  if (navigator.share) {
+    console.log("Using navigator.share");
+    navigator
+      .share({
+        title: title,
+        url: url,
+      })
+      .then(() => {
+        console.log("Share successful");
+      })
+      .catch((error) => {
+        console.error("Share failed:", error);
+      });
+  } else {
+    console.log("Navigator.share not available, trying clipboard");
+    // Fallback: copy to clipboard
+    navigator.clipboard
+      .writeText(url)
+      .then(function () {
+        console.log("Link copied to clipboard");
+        alert("Link copied to clipboard: " + url);
+      })
+      .catch(function (err) {
+        console.error("Failed to copy:", err);
+        // Fallback: open a new window or something
+        console.log("Opening mailto");
+        window.open(
+          "mailto:?subject=" +
+            encodeURIComponent(title) +
+            "&body=" +
+            encodeURIComponent(url),
+        );
+      });
+  }
+}

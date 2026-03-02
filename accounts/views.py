a3e52a34903def033a08d700a4b76418fb74d789
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2026-03-02 09:17:49 +0000

    Create profile_detail_view() to handle display of user detail view

--- accounts/views.py
+++ accounts/views.py
@@ -17,3 +17,14 @@ def profile_update(request):
         form = ProfileUpdateForm(instance=request.user)
 
     return render(request, "account/profile_update.html", {"form": form})
+
+
+def profile_detail_view(request, username):
+    profile_user = get_object_or_404(CustomUser, username=username)
+    user_blogs = profile_user.blog_set.all().order_by("-date")
+
+    return render(
+        request,
+        "account/profile_detail.html",
+        {"profile_user": profile_user, "user_blogs": user_blogs},
+    )
